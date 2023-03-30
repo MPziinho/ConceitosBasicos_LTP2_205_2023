b2b0a797b7f377e825527c7bc2df035dfb5c198c
Aula <Aula@CANP-LABTI7-C01>
2023-03-30 15:38:17 -0300

    Adicionar arquivos de projeto.

---
+++ conceitosBasicos/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Hello, World!");
+string nome = "Seu nome";
+Console.WriteLine("Seja bem vindo(a)" + nome);
+string cidade = "Volta Redonda";
+Console.WriteLine($"eu gosto de {cidade}");
+
+
+float altura = 1.83f;
+float peso = 60.5f;
+double alturaN = altura * 000000001;
+Console.WriteLine($"sua altura é {altura}");
+Console.WriteLine($"se voce pudesse ser o homem formiga, teria a altura" {alturaN}nm");
+Console.WriteLine($"o peso informado foi {peso}");
